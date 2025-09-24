f484cede9034e188b66a10269e1baf2450930e1b
VENKATARAGHAVAN T <56423301+VenkataRaghavan-Automationtestlab@users.noreply.github.com>
2025-09-24 23:48:38 +0530

    Add files via upload

---
+++ PW_POM/src/main/java/pages/CheckoutCompletePage.java
@@ -0,0 +1,42 @@
+package pages;
+
+import com.microsoft.playwright.*;
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class CheckoutCompletePage {
+	@SuppressWarnings("unused")
+	private final Page page;
+	// Locators
+	private Locator SauceLabsbagname;
+	private Locator shippingdetails;
+	private Locator finishBttn;
+	private Locator ThankyouMsg;
+
+	public CheckoutCompletePage(Page page) {
+		this.page = page;
+		SauceLabsbagname = page.locator("text=Sauce Labs Backpack");
+		shippingdetails = page.locator("(//*[@class='summary_value_label'])[2]");
+		finishBttn = page.locator("[class='btn_action cart_button']");
+		ThankyouMsg = page.locator("//*[@id='checkout_complete_container']/h2");
+	}
+
+	// Actions
+	public void ValidateDescription() {
+		assertThat(SauceLabsbagname).isVisible();
+		assertThat(SauceLabsbagname).containsText("Sauce Labs Backpack");
+	}
+
+	public void ValidateShippingInfo() {
+		assertThat(shippingdetails).isVisible();
+		assertThat(shippingdetails).containsText("FREE PONY EXPRESS DELIVERY!");
+	}
+
+	public void ClickFinishBttn() {
+		finishBttn.click();
+	}
+
+	public void ValidateThanksMsg() {
+		assertThat(ThankyouMsg).isVisible();
+		assertThat(ThankyouMsg).containsText("THANK YOU FOR YOUR ORDER");
+	}
+}
